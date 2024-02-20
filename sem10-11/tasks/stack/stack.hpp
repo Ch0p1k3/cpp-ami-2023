b54e7e90bbd536ed3bad3dbe35e7dabe37a2383e
Ch0p1k3 <ch00p.228@gmail.com>
2024-02-20 10:40:06 +0300

    Stack task

--- sem10-11/tasks/stack/stack.hpp
+++ sem10-11/tasks/stack/stack.hpp
@@ -7,21 +7,70 @@ public:
     using Value = uint64_t;
 
 public:
-    Stack();
+    Stack() = default;
 
-    Stack(const Stack& other);
+    Stack(const Stack& other) {
+        CopyStack(other);
+    }
 
-    Stack& operator=(const Stack& other);
+    Stack& operator=(const Stack& other) {
+        if (this == &other) {
+            return *this;
+        }
+        Clean();
+        CopyStack(other);
+        return *this;
+    }
 
-    void Push(Value value);
+    void Push(Value value) {
+        Node* new_top = new Node{value, top_};
+        top_ = new_top;
+    }
 
-    Value Top() const;
+    Value Top() const {
+        if (Empty()) {
+            return {};
+        }
+        return top_->value;
+    }
 
-    Value Pop();
+    Value Pop() {
+        if (Empty()) {
+            return {};
+        }
+        Value res = top_->value;
+        Node* new_top = top_->next;
+        delete top_;
+        top_ = new_top;
+        return res;
+    }
 
-    bool Empty() const;
+    bool Empty() const {
+        return top_ == nullptr;
+    }
 
-    ~Stack();
+    ~Stack() {
+        Clean();
+    }
+
+private:
+    void Clean() {
+        while (!Empty()) {
+            Pop();
+        }
+    }
+
+    void CopyStack(const Stack& other) {
+        Stack temp;
+        Node* other_top = other.top_;
+        while (other_top) {
+            temp.Push(other_top->value);
+            other_top = other_top->next;
+        }
+        while (!temp.Empty()) {
+            Push(temp.Pop());
+        }
+    }
 
 private:
     struct Node {
@@ -30,5 +79,5 @@ private:
     };
 
 private:
-    /* some fields */
+    Node* top_{nullptr};
 };
